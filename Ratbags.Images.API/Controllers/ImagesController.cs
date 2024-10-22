015dbdbf444f483a1d8ea3f5c17c9956804dce61
Adrian Emmott <adrian.emmott@gmail.com>
2024-10-22 15:06:21 +0100

    Added post to send image

--- Ratbags.Images.API/Controllers/ImagesController.cs
+++ Ratbags.Images.API/Controllers/ImagesController.cs
@@ -1,53 +1,80 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
 using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
 
-namespace Ratbags.Images.API.Controllers
+namespace Ratbags.Images.API.Controllers;
+
+[ApiController]
+[Route("api/images")]
+public class ImagesController : ControllerBase
 {
-    [ApiController]
-    [Route("api/images")]
-    public class ImagesController : ControllerBase
+
+    private readonly ILogger<ImagesController> _logger;
+
+    public ImagesController(ILogger<ImagesController> logger)
     {
-        
-        private readonly ILogger<ImagesController> _logger;
+        _logger = logger;
+    }
 
-        public ImagesController(ILogger<ImagesController> logger)
+    [HttpGet("{filename}")]
+    [ProducesResponseType(typeof(File), (int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.NotFound)]
+    [SwaggerOperation(Summary = "Gets an image",
+    Description = "Returns an image by filename, if it exists in wwwroot")]
+    public IActionResult Get(string filename)
+    {
+        // makes sure the images folder matches case of the actual folder
+        // as docker (linux) won't like discrepencies
+        var filepath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", filename);
+
+        if (!System.IO.File.Exists(filepath))
         {
-            _logger = logger;
+            return NotFound();
         }
 
-        [HttpGet("{filename}")]
-        [SwaggerOperation(Summary = "Gets an image",
-        Description = "Returns an image if it exists in wwwroot")]
-        public IActionResult Get(string filename)
+        var image = System.IO.File.OpenRead(filepath);
+        string mimeType = GetMimeType(filename);
+
+        return File(image, mimeType);
+    }
+
+
+    // TODO get authorise working
+    [HttpPost]
+    [ProducesResponseType((int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    [SwaggerOperation(Summary = "Creates an image in wwwroot/images",
+    Description = "Creates an image wwwroot/images")]
+    public async Task<IActionResult> Post()
+    {
+        var image = Request.Form.Files.FirstOrDefault();
+
+        if (image != null && image.Length > 0)
         {
-            // makes sure the images folder matches case of the actual folder
-            // as docker (linux) won't like discrepencies
-            var filepath = Path.Combine("wwwroot", "images", filename);
+            var filename = image.FileName;
 
-            if (!System.IO.File.Exists(filepath)) 
-            {
-                return NotFound();
-            }
+            var filepath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", filename);
 
-            var image = System.IO.File.OpenRead(filepath);
-            string mimeType = GetMimeType(filename);
+            using var stream = new FileStream(filepath, FileMode.Create);
+            await image.CopyToAsync(stream); // Copy the uploaded file to the stream
 
-            return File(image, mimeType);
+            //return Ok(new { message = $"Image uploaded:", filename = filename });
+            return Ok(new { message = $"Image uploaded: {filename}" });
         }
 
-        // TODO HttpPost
+        return BadRequest("Could not upload image");
+    }
 
-        private string GetMimeType(string filename)
+    private string GetMimeType(string filename)
+    {
+        var provider = new FileExtensionContentTypeProvider();
+
+        if (!provider.TryGetContentType(filename, out var contentType))
         {
-            var provider = new FileExtensionContentTypeProvider();
-
-            if (!provider.TryGetContentType(filename, out var contentType))
-            {
-                contentType = "application/octet-stream";
-            }
-
-            return contentType;
+            contentType = "application/octet-stream";
         }
+
+        return contentType;
     }
 }
